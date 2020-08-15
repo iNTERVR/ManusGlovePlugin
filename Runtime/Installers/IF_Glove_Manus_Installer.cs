005cec2afa8e4bf193be9748e6754e7a841d582b
ckbang <ckbang7@gmail.com>
2020-08-15 16:37:05 +0900

    Release 0.1.0

---
+++ Runtime/Installers/IF_Glove_Manus_Installer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace InterVR.IF.Glove.Plugin.Manus.Installer
+{
+    [CreateAssetMenu(fileName = "IF_Glove_Manus_Settings", menuName = "InterVR/IF/Plugin/Glove/Manus/Settings")]
+    public class IF_Glove_Manus_Installer : ScriptableObjectInstaller<IF_Glove_Manus_Installer>
+    {
+#pragma warning disable 0649
+        [SerializeField]
+        Settings settings;
+#pragma warning restore 0649
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(settings).IfNotBound();
+        }
+
+        [Serializable]
+        public class Settings
+        {
+            public string Name = "IF Manus Glove Plugin Installer";
+        }
+    }
+}
---
+++ Runtime/Installers/IF_Glove_Manus_MonoInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Zenject;
+
+namespace InterVR.IF.Glove.Plugin.Manus.Installer
+{
+    public class IF_Glove_Manus_MonoInstaller : MonoInstaller<IF_Glove_Manus_MonoInstaller>
+    {
+        public List<ScriptableObjectInstaller> settings;
+
+        public override void InstallBindings()
+        {
+            var settingsInstaller = settings.Cast<IInstaller>();
+            foreach (var installer in settingsInstaller)
+            {
+                Container.Inject(installer);
+                installer.InstallBindings();
+            }
+        }
+    }
+}
